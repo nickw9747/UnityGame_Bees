ada616dc1fc22a51e545f5a47d4b7da1ed9cf2e9
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-12 22:05:09 +1030

    setting up tests

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class InputManager : Singleton<InputManager>
 {
     // Start is called before the first frame update
     void Start()
