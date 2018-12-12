ab690545a2a1228e378d141caccbd4a3e27677a1
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-13 06:35:27 +1030

    copied pc files

---
+++ Assets/Scripts/Terrain/InvisibleWallEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class InvisibleWallEditor : MonoBehaviour {
+
+    [SerializeField]
+    private Color gizmoColour = Color.blue;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = gizmoColour;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+}
