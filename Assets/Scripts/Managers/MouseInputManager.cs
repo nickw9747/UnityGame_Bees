215ae043f2f889107161e0216985de3d5ecb0d88
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-14 07:33:10 +1030

    mouse world position test

--- Assets/Scripts/Managers/MouseInputManager.cs
+++ Assets/Scripts/Managers/MouseInputManager.cs
@@ -6,7 +6,7 @@ public class MouseInputManager : Singleton<MouseInputManager>
     private Camera mainCamera;
 
     [SerializeField]
-    private LayerMask layerMask;
+    private LayerMask layerMask = 1;
     const float MAX_DISTANCE = 100f;
 
     public Vector3 MouseWorldPosition { get; private set; }
@@ -26,6 +26,8 @@ public class MouseInputManager : Singleton<MouseInputManager>
 
         if (Physics.Raycast(ray, out RaycastHit hitInfo, MAX_DISTANCE, layerMask)) {
             MouseWorldPosition = hitInfo.point;
+            //Debug.DrawRay(ray.origin, ray.direction * MAX_DISTANCE);
+            //Debug.DrawRay(MouseWorldPosition, Vector3.up * 20, Color.blue);
         }
     }
 }
