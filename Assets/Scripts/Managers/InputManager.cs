5733a126e5fd35c09186d2da640b55671c47d55b
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-13 21:11:06 +1030

    input manager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class InputManager : Singleton<InputManager>
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class InputManager : Singleton<InputManager> {
+
+    private Vector3 moveDirection;
+
+    public Vector3 MovementDirection { get; private set; }
+    public bool Jump { get; private set; }
+    public bool Sprint { get; private set; }
+
+    public Action OnActionPress;
+
+    private void Update() {
+        MovementInput();
+
+        Jump = CrossPlatformInputManager.GetButton("Jump");
+        Sprint = CrossPlatformInputManager.GetButton("Sprint");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void MovementInput() {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+
+        MovementDirection = new Vector3(horizontal, 0, vertical);
+    }
+
+    private void CheckAction() {
+        if (CrossPlatformInputManager.GetButtonDown("Action") && OnActionPress != null) {
+            OnActionPress();
+        }
     }
 }
