dbae49f201d2ef51d8b00e54b431fd485ff65125
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-15 17:55:44 +1030

    trying unit tests

--- Assets/Scripts/Tests/Editor/TestHealth.cs
+++ Assets/Scripts/Tests/Editor/TestHealth.cs
@@ -3,5 +3,10 @@ using System.Collections;
 using NUnit.Framework;
 
 public class TestHealth {
-    
+    [Test]
+    public void TestDamage_DamageWithoutDeath() {
+        Health health = NSubstitute.Substitute.For<Health>();
+
+        //health.
+    }
 }
--- Assets/Scripts/Tests/Editor/TestPlayerMovement.cs
+++ Assets/Scripts/Tests/Editor/TestPlayerMovement.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 
 public class TestPlayerMovement
 {
+    public enum Directions { Forward, Backward, Left, Right };
+
+    public Vector3[] directions = { Vector3.forward, Vector3.back, Vector3.right, Vector3.left };
+
     [Test]
-    public void TestGroundedMovement_MoveLeftFromStationary(
-        [Values(1, 2, 3)] int moveAmount
+    public void TestGroundedMovement_MoveFromStationary(
+        [Values(-1, 0, 1)] int moveAmount,
+        [Values()] Directions direction
         ) {
         PlayerMovementInner pmi = new PlayerMovementInner();
 
         Vector3 currentMoveDirection = Vector3.zero;
 
-        pmi.GroundedMovement(ref currentMoveDirection, Vector3.left, moveAmount);
+        pmi.GroundedMovement(ref currentMoveDirection, directions[(int)direction], moveAmount);
 
-        Assert.AreEqual(currentMoveDirection, Vector3.left * moveAmount);
+        Assert.AreEqual(currentMoveDirection, directions[(int)direction] * moveAmount);
     }
 
+    [Test]
+    public void TestGroundedMovement_MoveWhileMoving(
+        [Values(-1, 0, 1)] int moveAmount,
+        [Values()] Directions direction,
+        [Values()] Directions currentDirection
+        ) {
+        PlayerMovementInner pmi = new PlayerMovementInner();
 
+        Vector3 currentMoveDirection = directions[(int)currentDirection];
+
+        pmi.GroundedMovement(ref currentMoveDirection, directions[(int)direction], moveAmount);
+
+        Assert.AreEqual(currentMoveDirection, directions[(int)direction] * moveAmount);
+    }
 }
