4f886fc3bb1e11bc333984b52480ea2c4ddbdf3a
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-17 07:37:39 +1030

    moving with navmesh

---
+++ Assets/Scripts/Bees/BeeMoveController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BeeMoveController : MonoBehaviour {
+
+    private NavMeshAgent beeAgent;
+
+    [SerializeField]
+    private Transform targetTransform;
+    [SerializeField]
+    private float maxSpeed = 1.0f;
+
+    private void Awake() {
+        beeAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        beeAgent.speed = maxSpeed;
+        if (!beeAgent.pathPending) {
+            beeAgent.SetDestination(targetTransform.position);
+        }
+    }
+}
