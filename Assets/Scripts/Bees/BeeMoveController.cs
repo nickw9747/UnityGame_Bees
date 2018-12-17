85de713cb4ad0246b3bb6e7539dea1a2f248dd2e
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-17 18:32:52 +1030

    bee navmesh dev

--- Assets/Scripts/Bees/BeeMoveController.cs
+++ Assets/Scripts/Bees/BeeMoveController.cs
@@ -8,18 +8,29 @@ public class BeeMoveController : MonoBehaviour {
     private NavMeshAgent beeAgent;
 
     [SerializeField]
-    private Transform targetTransform;
+    private Transform targetTransform = null;
+
     [SerializeField]
     private float maxSpeed = 1.0f;
+    [SerializeField]
+    private float maxAngularSpeed = 10.0f;
+
+    [SerializeField]
+    private float trackingDelay = 2.0f;
+    private float trackingTimer = 0.0f;
 
     private void Awake() {
         beeAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
+        //trackingTimer += Time.deltaTime;
         beeAgent.speed = maxSpeed;
+        beeAgent.angularSpeed = maxAngularSpeed;
+
         if (!beeAgent.pathPending) {
             beeAgent.SetDestination(targetTransform.position);
+            //trackingTimer = 0.0f;
         }
     }
 }
