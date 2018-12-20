3258a54058207f5c9c279d010497a80abe6160bd
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-20 20:55:36 +1030

    bee controller sucks

--- Assets/Scripts/Bees/BeeMoveController.cs
+++ Assets/Scripts/Bees/BeeMoveController.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class BeeMoveController : MonoBehaviour {
 
     private NavMeshAgent beeAgent;
+    private Rigidbody rb;
 
     [SerializeField]
     private Transform targetTransform = null;
@@ -17,22 +18,37 @@ public class BeeMoveController : MonoBehaviour {
 
     [SerializeField]
     private float trackForSeconds = 2.0f;
-    private float trackingTimer = 0.0f;
+    private float timer = 0.0f;
+
+    [SerializeField]
+    private float zoomSpeed = 2.0f;
+
+    private bool tracking = true;
+    private bool zooming = false;
 
     private void Awake() {
         beeAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update() {
-        trackingTimer += Time.deltaTime;
-        beeAgent.speed = maxSpeed;
-        beeAgent.angularSpeed = maxAngularSpeed;
 
-        if (trackingTimer <= trackForSeconds) {
+        if (timer <= trackForSeconds && tracking) {
+            timer += Time.deltaTime;
+            beeAgent.speed = maxSpeed;
+            beeAgent.angularSpeed = maxAngularSpeed;
             TrackTarget();
         }
-        else {
-            ZoomToTarget();
+        else if (tracking) {
+            transform.rotation.SetLookRotation(targetTransform.position);
+
+        }
+        else if (!zooming) {
+            //beeAgent.isStopped = true;
+            beeAgent.enabled = false;
+            rb.useGravity = false;
+            ZoomForward();
+            zooming = true;
         }
     }
 
@@ -42,7 +58,12 @@ public class BeeMoveController : MonoBehaviour {
         }
     }
 
-    private void ZoomToTarget() {
-
+    private void ZoomForward() {
+        //transform.position += transform.forward * zoomSpeed * Time.deltaTime;
+        rb.velocity = transform.forward * zoomSpeed;
     }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(gameObject.name + " collided (trigger) with " + other.name);
+    } 
 }
