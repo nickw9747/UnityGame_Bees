db6644d15cea28efcf4b4777c81399475b33d568
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-22 19:54:23 +1030

    damage on hit dev

---
+++ Assets/Scripts/Health/DamageOnHit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnHit : MonoBehaviour
+{
+    [SerializeField]
+    private int damage = 1;
+
+    private Action<Collider> OnDealtDamage = delegate { };
+
+    private void OnTriggerEnter(Collider colliderHit) {
+        DealDamage(colliderHit);
+        OnDealtDamage(colliderHit);
+    }
+
+    private void DealDamage(Collider colliderHit) {
+        Health health = colliderHit.GetComponent<Health>();
+        if (health != null) {
+            health.TakeDamage(damage); 
+        }
+    }
+}
