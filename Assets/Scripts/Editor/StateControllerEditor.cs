bb2181ee5b059ee5126f5314e70c086cec05fd15
nickw9747 <45817106+nickw9747@users.noreply.github.com>
2018-12-29 19:20:13 +1030

    state controller custom editor

---
+++ Assets/Scripts/Editor/StateControllerEditor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(StateController))]
+public class StateControllerEditor : Editor {
+    SerializedProperty stateTreesProp;
+
+    private List<bool> foldoutList = new List<bool>();
+
+    private void OnEnable() {
+        stateTreesProp = serializedObject.FindProperty("stateTrees");
+        for (int i = 0; i < stateTreesProp.arraySize; i++) {
+            foldoutList.Add(false);
+        }
+    }
+
+    public override void OnInspectorGUI() {
+        //base.OnInspectorGUI();
+        //EditorGUILayout.PropertyField(stateTreesProp, new GUIContent("States:"), includeChildren: true);
+        //EditorGUILayout.BeginHorizontal();
+
+        StateTreeEntries();
+
+        if (GUILayout.Button("+")) {
+            stateTreesProp.InsertArrayElementAtIndex(stateTreesProp.arraySize);
+            foldoutList.Add(false);
+        }
+
+        //EditorGUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    private void StateTreeEntries() {
+        
+        for (int i = 0; i < stateTreesProp.arraySize; i++) {
+            var stateTreeProp = stateTreesProp.GetArrayElementAtIndex(i);
+            var stateProp = stateTreeProp.FindPropertyRelative("state");
+            var transitionBranchesProp = stateTreeProp.FindPropertyRelative("transitionBranches");
+
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUI.indentLevel++;
+
+            foldoutList[i] = EditorGUILayout.Foldout(foldoutList[i], "State " + i.ToString() + ": ");
+
+            if (foldoutList[i]) {
+                EditorGUILayout.PropertyField(stateProp, new GUIContent("State: "));
+
+                EditorGUILayout.Space();
+
+                EditorGUILayout.PropertyField(transitionBranchesProp, includeChildren: true);
+
+                EditorGUILayout.Space();
+                if (GUILayout.Button("Delete State")) {
+                    DeleteState(i);
+                }
+                
+            }
+
+            EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+        }
+    }
+
+    private void DeleteState(int index) {
+        stateTreesProp.DeleteArrayElementAtIndex(index);
+        foldoutList.RemoveAt(index);
+    }
+}
---
+++ Assets/Scripts/Editor/TransitionBranchDrawer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+//[CustomPropertyDrawer(typeof(TransitionBranch))]
+//public class TransitionBranchDrawer : PropertyDrawer {
+//    private SerializedProperty transitionsProp;
+//    private SerializedProperty nextStateIndexProp;
+
+//    public override void OnGUI(Rect position, SerializedProperty transitionBranchProp, GUIContent label) {
+//        EditorGUI.BeginProperty(position, label, transitionBranchProp);
+
+//        GetProperties(transitionBranchProp);
+
+//        //EditorGUILayout.BeginVertical();
+
+//        for (int i = 0; i < transitionsProp.arraySize; i++) {
+//            EditorGUI.indentLevel++;
+
+//            var prop = transitionsProp.GetArrayElementAtIndex(i);
+//            EditorGUI.PropertyField(position, prop);
+
+//            if (GUI.Button(position, "-")) {
+//                Debug.Log("Remove Entry");
+//            }
+
+//            EditorGUI.indentLevel--;
+//        }
+        
+//        if (GUI.Button(position, "New transition")) {
+//            transitionsProp.InsertArrayElementAtIndex(transitionsProp.arraySize);
+//        }
+
+//        //EditorGUILayout.EndVertical();
+//        //base.OnGUI(position, transitionBranchProp, label);
+
+//        EditorGUI.EndProperty();
+//    }
+
+//    private void GetProperties(SerializedProperty transitionBranchProp) {
+//        transitionsProp = transitionBranchProp.FindPropertyRelative("transitions");
+//        nextStateIndexProp = transitionBranchProp.FindPropertyRelative("nextStateIndex");
+//    }
+
+//    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+//        return base.GetPropertyHeight(property, label) + 20;
+//    }
+//}
